11da69cb43e8a793b28ea6b2635d817c227f1dd2
JiangBowen0008 <jbw0008@outlook.com>
2022-02-26 16:56:51 -0800

    including pangolin in installation script

--- build.sh
+++ build.sh
@@ -6,22 +6,29 @@ cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
 make -j
 
-cd ../../g2o
-
 echo "Configuring and building Thirdparty/g2o ..."
 
+cd ../../g2o
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
 make -j
 
-cd ../../../
+
+echo "Configuring and building Thirdparty/Pangolin ..."
+cd ../../Pangolin-0.6
+sh ./scripts/install_prerequisites.sh --dry-run recommended
+mkdir build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release
+make -j
 
 echo "Uncompress vocabulary ..."
 
-cd Vocabulary
+cd ../../../Vocabulary
 tar -xf ORBvoc.txt.tar.gz
 cd ..
+
 
 echo "Configuring and building ORB_SLAM2 ..."
 
