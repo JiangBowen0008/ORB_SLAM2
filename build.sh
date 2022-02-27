025b8c98191fae8692c14b2b9d7fbbcdbddb97c2
JiangBowen0008 <jbw0008@outlook.com>
2022-02-26 18:21:42 -0800

    script change

--- build.sh
+++ build.sh
@@ -15,18 +15,17 @@ cmake .. -DCMAKE_BUILD_TYPE=Release
 make -j
 
 echo "Configuring and building Thirdparty/glew-2.1.0 ..."
-cd ../../glew-2.1.0
-mkdir build
-cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
+cd ../../glew-2.1.0/build
+cmake ./cmake -DCMAKE_BUILD_TYPE=Release
 make install -j
 
 echo "Configuring and building Thirdparty/Pangolin ..."
 cd ../../Pangolin-0.6
 sh ./install_prerequisites.sh -m apt all
+mkdir build
 cd build
-cmake ./cmake
-make install -j
+cmake .. -DCMAKE_BUILD_TYPE=Release
+make -j
 
 echo "Uncompress vocabulary ..."
 
